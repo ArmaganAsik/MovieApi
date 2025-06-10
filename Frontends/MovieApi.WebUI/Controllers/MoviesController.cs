97bd7388a8bd89c1ce965b77ad57d83ca59f61ad
mustafaarmaganasik@gmail.com <mustafaarmaganasik@gmail.com>
2025-06-10 23:01:58 +0300

    Movie Detail was completed

---
+++ Frontends/MovieApi.WebUI/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.DTO.DTOs.MovieDTOs;
+using Newtonsoft.Json;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MoviesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> MovieList()
+        {
+            ViewBag.v1 = "Film Listesi";
+            ViewBag.v2 = "Ana Sayfa";
+            ViewBag.v3 = "Tüm Filmler";
+
+            HttpClient client = _httpClientFactory.CreateClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7253/api/Movies");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string jsonData = await responseMessage.Content.ReadAsStringAsync();
+                List<ResultMovieDTO> values = JsonConvert.DeserializeObject<List<ResultMovieDTO>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> MovieDetail(int id)
+        {
+            id = 0;
+            return View();
+        }
+    }
+}
---
+++ Frontends/MovieApi.WebUI/ViewComponents/UserLayoutWebUIViewComponents/_UserLayoutWebUINavbarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MovieApi.WebUI.ViewComponents.UserLayoutWebUIViewComponents
+{
+    public class _UserLayoutWebUINavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
